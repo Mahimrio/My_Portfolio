f7202ca232a19be79c66a0dd31f9d04396f975a2
mahimrio <mahim.cse.20230104015@aust.edu>
2026-04-18 03:47:15 +0600

    Issue 5.2: Build About Me and Skills sections with animated progress/marquee

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import Layout from './components/Layout';
 import BackgroundCanvas from './components/canvas/BackgroundCanvas';
 import SmoothScroll from './components/SmoothScroll';
-import Reveal from './components/animations/Reveal';
 import HeroSection from './components/sections/HeroSection';
+import AboutSection from './components/sections/AboutSection';
 
 function App() {
   return (
@@ -11,12 +11,7 @@ function App() {
       <SmoothScroll>
         <Layout>
           <HeroSection />
-          
-          <section id="about" style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-             <Reveal direction="left">
-               <h2>Scroll Animations are Working!</h2>
-             </Reveal>
-          </section>
+          <AboutSection />
         </Layout>
       </SmoothScroll>
     </>
